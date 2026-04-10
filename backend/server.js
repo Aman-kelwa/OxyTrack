db1675f607c91612455f8381f33a7429db0fde57
Aman-kelwa <akelwa23@gmail.com>
2026-04-11 00:44:07 +0530

    setup frontend url

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://oxy-track.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
